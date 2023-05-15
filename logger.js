674d646cbfda31289cf5809d61329a9c44910a5c
Park Lu <park.lu@solartech.com.tw>
2023-05-15 20:32:38 +0800

    use the basic logger

--- logger.js
+++ logger.js
@@ -1,6 +1,34 @@
 import winston, { createLogger, format, transports } from 'winston';
 
 const logger = createLogger({
-  transports: [new transports.Console()],
-  format: format.combine(format.colorize({ all: true }), format.simple()),
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  // defaultMeta: { service: 'your-service-name' },
+  transports: [
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/combined.log' }),
+  ],
 });
+
+//
+// If we're not in production then **ALSO** log to the `console`
+// with the colorized simple format.
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    })
+  );
+}
+
+// logger.log('info', 'Hello, this is a raw logging event');
+// logger.warn('eeeee');
+// logger.error('eeeee', { hello: 'world' });
